fd674bcee5d2e2ec31a094f26df8e94abfea6ca1
imujinu <wlsdnrhdwkd@gmail.com>
2025-06-04 15:19:14 +0900

    test

--- basic_syntax/2.basic_command.sh
+++ basic_syntax/2.basic_command.sh
@@ -57,4 +57,16 @@ git reset head^
 # 이미 push된 commit 사항 되돌리기(완전한 취소X) commitID 생성성
 git revert 커밋ID
 
+# 작업중인 사항을 임시 저장
+git stash
+
+# 마지막으로 저장한 사항을 꺼내서 적용
+git stash pop
+
+# 저장한 작업목록 조회
+git stash list
+
+# 저장한 목록 전체 삭제
+git stash clear
+
 
